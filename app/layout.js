38c6fda3f3bbeee25648a5445f4431d0c22cb549
Shawn Pearson <1157wcl@gmail.com>
2025-01-03 15:23:32 -0500

    radix

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+// evaluate: radix-ui
+// import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+import Header from "../components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +24,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Theme>
+          <Header />
+          {children}
+        </Theme>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>homepage</h1>
-    </div>
-  );
+  return <div>Home</div>;
 }
